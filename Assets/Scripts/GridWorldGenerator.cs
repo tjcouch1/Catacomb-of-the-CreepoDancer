1f98dbcb14f669543a61cdf955cc4f0a78eaddab
Clark Chambers <clarkchambers94@gmail.com>
2017-02-03 14:17:14 -0600

    Changed gitignore to reflect another common folder name I use

---
+++ Assets/Scripts/GridWorldGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridWorldGenerator : MonoBehaviour {
+
+	public int width;
+	public int height;
+	public List<GameObject> PrefabTiles;
+
+	// Use this for initialization
+	// void Start () {
+		// 
+	// }
+
+	void Awake() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

54212a5b6d60cbb98884e7113ff78c92d42945b4
Clark Chambers <clarkchambers94@gmail.com>
2017-02-04 15:37:38 -0600

    Added a level and a little guy to test moving around. His input will be changed to raycasts checking for collision on updates.

---
+++ Assets/Scripts/MoveAround.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAround : MonoBehaviour 
+{
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+			transform.position += new Vector3(1f, 0f, 0f);
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			transform.position += new Vector3(-1f, 0f, 0f);
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			transform.position += new Vector3(0f, 1f, 0f);
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			transform.position += new Vector3(0f, -1f, 0f);
+		}
+	}
+}

c79c2a563ceddddd191f434a979f2f0fdd603d32
tjc0014 <tjc0014@uah.edu>
2017-02-07 22:11:51 -0600

    Added BlueSlime and AI

---
+++ Assets/Scripts/EnemyAI/BlueSlimeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Dirs = GridMovement.Directions;
+
+[RequireComponent(typeof(GridUpdateSubscriber))]
+[RequireComponent(typeof(GridMovement))]
+public class BlueSlimeController : MonoBehaviour {
+
+	//path and current position in path
+	int step = 0;
+	Dirs[] dirs = { Dirs.DOWN, Dirs.NULL, Dirs.UP, Dirs.NULL };
+
+	// Utility GridMovement component
+	GridMovement gm;
+
+	// Use this for initialization
+	void Start () 
+	{
+		gm = GetComponent<GridMovement>();
+		GridUpdateSubscriber gus = GetComponent<GridUpdateSubscriber>();
+		gus.SetSubscriberMethod(new GridUpdateSubscriber.SubscriberDelegate(SubUpdate));
+	}
+
+	void SubUpdate() 
+	{
+		if (dirs[step] != Dirs.NULL)
+		{
+			bool attacked = gameObject.GetComponent<EnemyComponent>().Attack(dirs[step]); // Attack in the given direction
+
+			// returns false is there was no enemy to attack.
+			if (!attacked)
+				if (gm.CanMove(dirs[step]))
+					transform.position += (Vector3) GridMovement.DirTable[dirs[step]];
+		}
+		step++;
+		step %= dirs.Length;
+		Debug.Log("Delegate System works!");
+	}
+}
--- Assets/Scripts/EnemyAI/EnemyComponent.cs
+++ Assets/Scripts/EnemyAI/EnemyComponent.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Dirs = GridMovement.Directions;
+
 public class EnemyComponent : MonoBehaviour {
 
 	[Range(0,100)]
@@ -13,6 +15,8 @@ public class EnemyComponent : MonoBehaviour {
 	[Range(0,10)]
 	public int attackPower;
 	int health;
+
+	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
@@ -27,7 +31,32 @@ public class EnemyComponent : MonoBehaviour {
 	public void Hit(int dmg) {
 		health -= dmg;
 		if(health <= 0) {
-			Destroy(gameObject);
+			Die();
 		}
+	}
+
+	public void Die()
+	{
+		//add coins to player
+		Destroy(gameObject);
+	}
+
+	public bool Attack(Dirs dir)
+	{
+		// Shoot out a ray to check for a collision with the level layer. 
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, 			// origin
+			GridMovement.DirTable[dir], 	// Lookup table (direction)!
+			1f, 							// Only 1 unit Grid
+			LayerMask.GetMask("Player")); // Only on this layer
+
+		// If a collider exists, we found an enemy
+		if(hit.collider != null){
+			// Debug.Log("FOUND AN ENEMY!!!");
+			PlayerHealthController phc = hit.collider.gameObject.GetComponent<PlayerHealthController>();
+			if(phc != null) {
+				phc.DealDamage(attackPower);
+			}
+		}
+		return (hit.collider != null);
 	}
 }

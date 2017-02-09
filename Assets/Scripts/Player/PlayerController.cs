9233e7d004d4f7ebd56cc7826b26a478f6f70010
tjc0014 <tjc0014@uah.edu>
2017-02-08 21:52:17 -0600

    Revert "CAUTION:NOT STABLE Added translation, possibly messed stuff up reeeeeel bad"
    This reverts commit c3fc3c4dea47fc1611beb2fb0528161fe4bf6e15.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -88,8 +88,6 @@ public class PlayerController : MonoBehaviour {
 			// returns false is there was no enemy to attack.
 			if(!attacked){
 				if(gm.CanMovePlayer(dir)) {
-					GridSpriteTranslate gst = GetComponentInChildren<GridSpriteTranslate>();
-					gst.StartTranslation();
 					transform.position += (Vector3)GridMovement.DirTable[dir];
 				}
 			}

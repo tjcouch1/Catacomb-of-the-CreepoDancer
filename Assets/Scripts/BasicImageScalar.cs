8873aa50edafb62347bc20fe2b2ca2ee34bdbc3c
Clark Chambers <clarkchambers94@gmail.com>
2017-02-10 15:29:38 -0600

    Added a turn timer to bottom right of UI

---
+++ Assets/Scripts/BasicImageScalar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicImageScalar : MonoBehaviour {
+
+	float start_x;
+	RectTransform rt;
+
+	// Use this for initialization
+	void Awake() {
+		rt = GetComponent<RectTransform>();
+		start_x = rt.sizeDelta.x;
+	}
+	
+	// Should be from 0-1
+	public void SetXScale(float scl) {
+		rt.sizeDelta = new Vector2((scl * start_x), rt.sizeDelta.y);
+	}
+}
--- Assets/Scripts/WorldTimer.cs
+++ Assets/Scripts/WorldTimer.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 [RequireComponent(typeof(SimultaneousUpdater))]
 public class WorldTimer : MonoBehaviour {
 
+	public GameObject timerBar;
+
 	[Range(0,2)]
 	public float turnTime;
+	BasicImageScalar scl;
 
 	float current;
 	SimultaneousUpdater smu;
@@ -18,12 +21,18 @@ public class WorldTimer : MonoBehaviour {
 		current = 0;
 		smu = GetComponent<SimultaneousUpdater>();
 		cm = GetComponent<CoinMultiplier>();
+		scl = timerBar.GetComponent<BasicImageScalar>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		// Update time
 		current += Time.deltaTime;
+
+		// Update UI
+		scl.SetXScale((turnTime - current) / turnTime);
+
 		if(current > turnTime) {
 			ForceUpdate();
 		}
